bef6d974ab8bffd0aaa201570b5555e24a419984
Kyle Talbot <kyle.talbot@cultivatebrands.com>
2025-03-26 17:15:00 -0500

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 
-use ::bevy::{
+use std::{f32::consts::PI, ops::Range, time::Duration};
+use bevy::{
     prelude::*,
     ecs::{event::Event, schedule::ScheduleLabel},
     time::common_conditions::on_timer,
-
+    utils::HashSet,
+    window::PrimaryWindow,
+};
+use bevy_prototype_lyon::{
+    entity::ShapeBundle,
+    prelude::{
+        tess::{geom::Rotation, math::Angle},
+        *,
+    },
+    shapes::Polygon,
 };
 fn main() {
     println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Roid UP".to_string(),
+                resolution: (800.0, 600.0).into(),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
